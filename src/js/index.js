1b3a1fbf7af16525f268a0a919c89ef67e441105
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-04-08 09:33:05 +0300

    fix: delete folder 'app'

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-import App from './app/app.js';
+import { header } from './components/header/header';
+import { footer } from './components/footer/footer';
+import { main } from './components/main/main';
+
+const webix = require('webix/webix.js');
+
+const App = webix.ready(() => {
+  webix.ui({
+    view: 'layout',
+    rows: [
+      header,
+      main,
+      footer,
+    ],
+  });
+});
