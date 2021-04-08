3e7248b0e8ba8e1d15f0d57d0e196d4f8eddebdc
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-04-08 19:00:40 +0300

    feat: add validateForm function

--- src/js/components/main/form/form.js
+++ src/js/components/main/form/form.js
@@ -1,3 +1,6 @@
+import { validate } from "@babel/types";
+const webix = require('webix/webix.js');
+
 const form = {
   view: 'form',
   width: 300,
@@ -6,25 +9,50 @@ const form = {
     { template: 'edit films', type: 'section' },
     { view: 'text', label: 'Title', name: "title" },
     { view: 'text', label: 'Year', type:'number', name: "year" },
-    { view: 'text', label: 'Date', type:'number', name: "date" },
     { view: 'text', label: 'Votes', type:'number', name: "votes" },
     { view: 'text', label: 'Rating', type:'number', name: "rating" },
     { margin: 10,
       cols: [
-        { view: 'button', value: 'Add new', css: 'webix_primary', },
-        {view: 'button', value: 'Clear', click: clearForm, }
+        { view: 'button', value: 'Add new', css: 'webix_primary',  click: addNewFilm},
+        { view: 'button', value: 'Clear', click: clearForm}
       ],
     },
     {},
   ],
+  rules:{
+    title: webix.rules.isNotEmpty,
+    year: function(value){return value > 1970},
+    votes: function(value){return value < 100000},
+    rating: function(value){return value != 0}
+  }
 };
 
-
 function clearForm(){
-  console.log(this);
   $$("editFilmsForm").clear();
 }
 
+function addNewFilm() {
+  const form = $$("editFilmsForm");
+
+  if ( validateForm(form) ){
+    console.log('true');
+  }else{
+    console.log('false');
+  }
+}
+
+function validateForm(form) {
+  if (form.validate()){
+    webix.message("All is correct");
+    return true;
+  } else{
+    webix.message({ type:"error", text:"Form data are invalid" });
+    return false;
+  }
+}
 
 
 export { form };
+
+
+
